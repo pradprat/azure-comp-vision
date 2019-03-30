9bc6b03681bd7c8968ee8056d1bec079e1cb9ea4
pradika pratama <33866097+pradprat@users.noreply.github.com>
2019-03-31 06:54:46 +0700

    fork

--- phpQS.php
+++ phpQS.php
@@ -1,29 +1,57 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>azure</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css">
-</head>
-<body>
 <?php
+/**----------------------------------------------------------------------------------
+* Microsoft Developer & Platform Evangelism
+*
+* Copyright (c) Microsoft Corporation. All rights reserved.
+*
+* THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+* EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES 
+* OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+*----------------------------------------------------------------------------------
+* The example companies, organizations, products, domain names,
+* e-mail addresses, logos, people, places, and events depicted
+* herein are fictitious.  No association with any real company,
+* organization, product, domain name, email address, logo, person,
+* places, or events is intended or should be inferred.
+*----------------------------------------------------------------------------------
+**/
+
+/** -------------------------------------------------------------
+# Azure Storage Blob Sample - Demonstrate how to use the Blob Storage service. 
+# Blob storage stores unstructured data such as text, binary data, documents or media files. 
+# Blobs can be accessed from anywhere in the world via HTTP or HTTPS. 
+#
+# Documentation References: 
+#  - Associated Article - https://docs.microsoft.com/en-us/azure/storage/blobs/storage-quickstart-blobs-php 
+#  - What is a Storage Account - http://azure.microsoft.com/en-us/documentation/articles/storage-whatis-account/ 
+#  - Getting Started with Blobs - https://azure.microsoft.com/en-us/documentation/articles/storage-php-how-to-use-blobs/
+#  - Blob Service Concepts - http://msdn.microsoft.com/en-us/library/dd179376.aspx 
+#  - Blob Service REST API - http://msdn.microsoft.com/en-us/library/dd135733.aspx 
+#  - Blob Service PHP API - https://github.com/Azure/azure-storage-php
+#  - Storage Emulator - http://azure.microsoft.com/en-us/documentation/articles/storage-use-emulator/ 
+#
+**/
 
 require_once 'vendor/autoload.php';
 require_once "./random_string.php";
+
 use MicrosoftAzure\Storage\Blob\BlobRestProxy;
 use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
 use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
 use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
 use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
+
 $connectionString = "DefaultEndpointsProtocol=https;AccountName=".getenv('ACCOUNT_NAME').";AccountKey=".getenv('ACCOUNT_KEY');
+
 // Create blob client.
 $blobClient = BlobRestProxy::createBlobService($connectionString);
+
 $fileToUpload = "HelloWorld.txt";
+
 if (!isset($_GET["Cleanup"])) {
     // Create container options object.
     $createContainerOptions = new CreateContainerOptions();
+
     // Set public access policy. Possible values are
     // PublicAccessType::CONTAINER_AND_BLOBS and PublicAccessType::BLOBS_ONLY.
     // CONTAINER_AND_BLOBS:
@@ -39,13 +67,17 @@ if (!isset($_GET["Cleanup"])) {
     // If this value is not specified in the request, container data is
     // private to the account owner.
     $createContainerOptions->setPublicAccess(PublicAccessType::CONTAINER_AND_BLOBS);
+
     // Set container metadata.
     $createContainerOptions->addMetaData("key1", "value1");
     $createContainerOptions->addMetaData("key2", "value2");
+
       $containerName = "blockblobs".generateRandomString();
+
     try {
         // Create container.
         $blobClient->createContainer($containerName, $createContainerOptions);
+
         // Getting local file so that we can upload it to Azure
         $myfile = fopen($fileToUpload, "w") or die("Unable to open file!");
         fclose($myfile);
@@ -56,12 +88,16 @@ if (!isset($_GET["Cleanup"])) {
         echo "<br />";
         
         $content = fopen($fileToUpload, "r");
+
         //Upload blob
         $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
+
         // List blobs.
         $listBlobsOptions = new ListBlobsOptions();
         $listBlobsOptions->setPrefix("HelloWorld");
+
         echo "These are the blobs present in the container: ";
+
         do{
             $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
             foreach ($result->getBlobs() as $blob)
@@ -72,6 +108,7 @@ if (!isset($_GET["Cleanup"])) {
             $listBlobsOptions->setContinuationToken($result->getContinuationToken());
         } while($result->getContinuationToken());
         echo "<br />";
+
         // Get blob.
         echo "This is the content of the blob uploaded: ";
         $blob = $blobClient->getBlob($containerName, $fileToUpload);
@@ -97,6 +134,7 @@ if (!isset($_GET["Cleanup"])) {
 } 
 else 
 {
+
     try{
         // Delete container.
         echo "Deleting Container".PHP_EOL;
@@ -119,5 +157,3 @@ else
 <form method="post" action="phpQS.php?Cleanup&containerName=<?php echo $containerName; ?>">
     <button type="submit">Press to clean up all resources created by this sample</button>
 </form>
-</body>
-</html>
