b697813f91218b75fcefa5a0e3da7a27c7c88331
pradika pratama <33866097+pradprat@users.noreply.github.com>
2019-03-31 06:52:10 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>azure</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css">
+</head>
+<body>
 <?php
 
 require_once 'vendor/autoload.php';
@@ -109,3 +119,5 @@ else
 <form method="post" action="phpQS.php?Cleanup&containerName=<?php echo $containerName; ?>">
     <button type="submit">Press to clean up all resources created by this sample</button>
 </form>
+</body>
+</html>
