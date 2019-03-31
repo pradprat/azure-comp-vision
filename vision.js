256ddea204816f08a2d7fc78f2c8884f40a1c623
pradika pratama <33866097+pradprat@users.noreply.github.com>
2019-03-31 16:19:00 +0700

    Update vision.js

--- vision.js
+++ vision.js
@@ -51,61 +51,101 @@ function processImage(sourceImageUrl) {
 };
 
 function getLink() {
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
+    // var xmlhttp = new XMLHttpRequest();
+    // xmlhttp.onreadystatechange = function() {
+    //     if (this.readyState == 4 && this.status == 200) {
 
-            var str=this.responseText;
-            str = str.substring(0, str.indexOf('\n'));
+    //         var str=this.responseText;
+    //         str = str.substring(0, str.indexOf('\n'));
 
-            var links = str.split("https://");
-            links.shift();
-            links.forEach(function(element) {
-                if(element.includes(".jpg"))
-                {
-                    document.getElementById("images-place").innerHTML +=
-                    "<div class='card m-3' style='width: 18rem;'>"+
-                        "<img src='https://"+element+"' class='card-img-top'>"+
-                        "<div class='card-body'>"+
-                            "<h5 class='card-title' image='https://"+element+"'>Card title</h5>"+
-                            "<p class='card-text' image='https://"+element+"'>"+processImage("https://"+element)+"</p>"+
-                        "</div>"+
-                        "<button class='btn btn-primary' type='button' data-toggle='collapse' data-target='#pic"+str.indexOf(element)+"' aria-expanded='false' aria-controls='pic"+str.indexOf(element)+"'>"+
-                            "Show JSON"+
-                        "</button>"+
+    //         var links = str.split("https://");
+    //         links.shift();
+    //         links.forEach(function(element) {
+    //             if(element.includes(".jpg"))
+    //             {
+    //                 document.getElementById("images-place").innerHTML +=
+    //                 "<div class='card m-3' style='width: 18rem;'>"+
+    //                     "<img src='https://"+element+"' class='card-img-top'>"+
+    //                     "<div class='card-body'>"+
+    //                         "<h5 class='card-title' image='https://"+element+"'>Card title</h5>"+
+    //                         "<p class='card-text' image='https://"+element+"'>"+processImage("https://"+element)+"</p>"+
+    //                     "</div>"+
+    //                     "<button class='btn btn-primary' type='button' data-toggle='collapse' data-target='#pic"+str.indexOf(element)+"' aria-expanded='false' aria-controls='pic"+str.indexOf(element)+"'>"+
+    //                         "Show JSON"+
+    //                     "</button>"+
 
-                        "<div class='collapse' id='pic"+str.indexOf(element)+"'>"+
-                            "<div image='https://"+element+"' class='card card-body'>"+
-                                "<p></p>"+
-                            "</div>"+
-                        "</div>"+
-                    "</div>";
-                    }
+    //                     "<div class='collapse' id='pic"+str.indexOf(element)+"'>"+
+    //                         "<div image='https://"+element+"' class='card card-body'>"+
+    //                             "<p></p>"+
+    //                         "</div>"+
+    //                     "</div>"+
+    //                 "</div>";
+    //                 }
                 
-            });
-        }
-    };
+    //         });
+    //     }
+    // };
 
-    xmlhttp.open("GET", "phpQS.php");
-    xmlhttp.send();
+    // xmlhttp.open("GET", "phpQS.php");
+    // xmlhttp.send();
+
+    $.get("phpQS.php", function( data ) 
+    {
+        var str=data;
+        str = str.substring(0, str.indexOf('\n'));
+
+        var links = str.split("https://");
+        links.shift();
+        links.forEach(function(element) 
+        {
+            if(element.includes(".jpg"))
+            {
+                document.getElementById("images-place").innerHTML +=
+                "<div class='card m-3' style='width: 18rem;'>"+
+                    "<img src='https://"+element+"' class='card-img-top'>"+
+                    "<div class='card-body'>"+
+                        "<h5 class='card-title' image='https://"+element+"'>Card title</h5>"+
+                        "<p class='card-text' image='https://"+element+"'>"+processImage("https://"+element)+"</p>"+
+                    "</div>"+
+                    "<button class='btn btn-primary' type='button' data-toggle='collapse' data-target='#pic"+str.indexOf(element)+"' aria-expanded='false' aria-controls='pic"+str.indexOf(element)+"'>"+
+                        "Show JSON"+
+                    "</button>"+
+
+                    "<div class='collapse' id='pic"+str.indexOf(element)+"'>"+
+                        "<div image='https://"+element+"' class='card card-body'>"+
+                            "<p></p>"+
+                        "</div>"+
+                    "</div>"+
+                "</div>";
+            }
+        });
+    });
 }
 
-function uploadImage(url) {
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
+function uploadImage(url) 
+{
+    // var xmlhttp = new XMLHttpRequest();
+    // xmlhttp.onreadystatechange = function() {
+    //     if (this.readyState == 4 && this.status == 200) {
 
-            var str=this.responseText;
-            $("#uploadStatus").text(str);
+    //         var str=this.responseText;
+    //         $("#uploadStatus").text(str);
             
-        }
-    };
+    //     }
+    // };
 
-    xmlhttp.open("GET", "upload_file.php?q=" + url);
-    xmlhttp.send();
+    // xmlhttp.open("GET", "upload_file.php?q=" + url);
+    // xmlhttp.send();
+
+    $.get("upload_file.php?q=" + url, function( data ) 
+    {
+        var str=data;
+        $("#uploadStatus").text(str);
+    });
 }
 
-function displayExampleJSON(json){
+function displayExampleJSON(json)
+{
     var strjson = JSON.stringify(json);
 
 }
